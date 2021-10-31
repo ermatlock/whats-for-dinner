3cbb09504970287ea1f4408bb120d6a0c6fb8299
Eric Matlock <ermatlock@gmail.com>
2021-10-31 00:21:12 -0400

    Complete iteration 1 functionality

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-var choiceSide = document.getElementById("#choice-side");
-var choiceMain = document.getElementById("#choice-main");
-var choiceDessert = document.getElementById("#choice-dessert");
+var choiceSide = document.getElementById("choice-side");
+var choiceMain = document.getElementById("choice-main");
+var choiceDessert = document.getElementById("choice-dessert");
 var letsCookButton = document.querySelector(".lets-cook");
 var cookpotGraphic = document.querySelector(".cookpot-graphic");
-// var radioButtonSelect = document.querySelectorAll(".radio");
 var youShouldMake = document.querySelector(".you-should-make");
 var randomFood = document.querySelector(".random-food");
 var clearButton = document.querySelector(".clear-button");
 
 letsCookButton.addEventListener("click", letsCook);
 clearButton.addEventListener("click", clearResult);
-// radioButtonSelect.addEventListener("change", selectChoice);
 
 function show(element) {
   element.classList.remove("hidden");
 };
+
 function hide(element) {
   element.classList.add("hidden");
 };
 
+function getRandom(array) {
+  return Math.floor(Math.random() * array.length);
+};
+
 function letsCook() {
   selectChoice();
-  // randomFood.innerText = "I Replaced You"
   hide(cookpotGraphic);
   show(randomFood);
   show(youShouldMake);
@@ -35,23 +37,12 @@ function clearResult() {
   hide(clearButton);
 };
 
-function getRandom(array) {
-  return Math.floor(Math.random() * array.length);
-};
-
-function getRandomFood(array) {
-  var randomFood = getRandom(array);
-
-}
-
-
-
 function selectChoice(choice) {
   if (choiceSide.checked) {
-  randomFood.innerText = getRandomFood(sides);
+  randomFood.innerText = `${sides[getRandom(sides)]}`;
   } else if (choiceMain.checked) {
-  randomFood.innerText = getRandomFood(mains);
+  randomFood.innerText = `${mains[getRandom(mains)]}`;
   } else if (choiceDessert.checked) {
-  randomFood.innerText = getRandomFood(desserts);
+  randomFood.innerText = `${desserts[getRandom(desserts)]}`;
   }
 };
